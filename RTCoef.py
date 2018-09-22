2800ca6356c85b967b81a2e0c0916a9a5f8abc38
ymh12345 <43492500+ymh12345@users.noreply.github.com>
2018-09-22 21:48:55 +0800

    222

--- RTCoef.py
+++ RTCoef.py
@@ -6,6 +6,7 @@ class RTCoef():
     def __init__(self, *icang, **media):
         """
         icang                   list of incident angle(unit is rad)
+        
         **media(key word argument)
             rho1, rho2          density
             beta1, beta2        S wave velocity
@@ -30,37 +31,40 @@ class RTCoef():
             self.isreal = False
             self.ic = np.arcsin(self.beta1 / self.beta2)
 
-        self.RCoef, self.TCoef = self.__calRTCoef()
-        #self.RCoef = np.abs(self.RCoef)
+        self.RCoef, self.TCoef, self.RE, self.TE = self.__calRTCoef()
         self.RC_Real = [i.real for i in self.RCoef]
         self.RC_Imag = [i.imag for i in self.RCoef]
         self.TC_Real = [i.real for i in self.TCoef]
         self.TC_Imag = [i.imag for i in self.TCoef]
+
     
     def __calRTCoef(self):
         """
         calculate RTCoef, and the formulas used here is from "Aki and Richards, Quantitative Seismolgy, 2002".
         """
-        RC = []
-        TC = []
+        RC = [] # reflect coefficient
+        TC = [] # transmission coefficient
+        RE = [] # reflect energy flux
+        TE = [] # transmission energy flux
         if self.isreal:
            for i in self.icang:
                tmp1 = self.rho1 * self.beta1 * np.cos(i)
                tmp2 = self.rho2 * self.beta2 * np.cos(self.__gettang(i))
                tmp3 = 2 * tmp1
-               RC.append((tmp1 - tmp2) / (tmp1 + tmp2))
-               TC.append(tmp3 / (tmp1 + tmp2))
+               R = (tmp1 - tmp2) / (tmp1 + tmp2)
+               RC.append(R)
+               T = tmp3 / (tmp1 + tmp2)
+               TC.append(T)
+               R_ratio = R ** 2
+               RE.append(R_ratio)
+               T_ratio = T ** 2 * self.rho2 * self.beta2 * np.cos(self.__gettang(i))\
+                         / (self.rho1 * self.beta1 * np.cos(i))
+               TE.append(T_ratio)
+               # print(T_ratio + R_ratio)
+               # T_ratio + R_ratio = 1
+
         else:
             for i in self.icang:
-                """
-                if i < self.ic:
-                    tmp1 = self.rho1 * self.beta1 * np.cos(i)
-                    tmp2 = self.rho2 * self.beta2 * np.cos(self.__gettang(i))
-                    tmp3 = 2 * tmp1
-                    RC.append(complex((tmp1 - tmp2) / (tmp1 + tmp2)))
-                    TC.append(complex(tmp3 / (tmp1 + tmp2)))
-                else:
-                    """
                 if self.beta2 * np.sin(i) / self.beta1 <= 1:
                     cosj2 = np.sqrt(1 - np.sin(self.__gettang(i)) ** 2)
                 else:
@@ -68,10 +72,18 @@ class RTCoef():
                 tmp1 = self.rho1 * self.beta1 * np.cos(i)
                 tmp2 = self.rho2 * self.beta2 * cosj2
                 tmp3 = 2 * tmp1
-                RC.append((tmp1 - tmp2) / (tmp1 + tmp2))
-                TC.append(tmp3 / (tmp1 + tmp2))
-                
-        return RC, TC
+                R = (tmp1 - tmp2) / (tmp1 + tmp2)
+                RC.append(R)
+                T = tmp3 / (tmp1 + tmp2)
+                TC.append(T)
+                R_ratio = R ** 2
+                RE.append(R_ratio.real)  
+                T_ratio = T ** 2 * self.rho2 * self.beta2 * cosj2 / (self.rho1 * self.beta1 * np.cos(i))
+                TE.append(T_ratio.real)
+                # print(R_ratio + T_ratio)
+                # Because of influence of float point number, this value is not equals 1 accurately, 
+                # but its imaginary are almost zero, e.g 1 - 5.55111e-17j et.al..
+        return RC, TC, RE, TE
 
     def __gettang(self, iang):
         """
@@ -88,18 +100,24 @@ class RTCoef():
 
     def plot(self):
         fig = plt.figure(figsize=(10, 4))
-        #plt.subplot(211)
+        plt.subplot(211)
         plt.plot(np.rad2deg(self.icang), self.RC_Real, color='red', label="Reflect_Real")
         plt.plot(np.rad2deg(self.icang), self.RC_Imag, color='red', linestyle="--", label="Reflect_Imag")
-        #plt.xlabel("Incident Angle")
-        #plt.ylabel("R/T coefficient")
-        #plt.legend()
-        #plt.subplot(212)
         plt.plot(np.rad2deg(self.icang), self.TC_Real, color='blue', label="Transmission_Real")
         plt.plot(np.rad2deg(self.icang), self.TC_Imag, color='blue', linestyle="--", label="Transmission_Imag")        
         plt.legend()
-        plt.xlabel("Incident Angle")
+        #plt.xlabel("Incident Angle")
         plt.ylabel("R/T coefficient")
+        
+        plt.subplot(212)
+        plt.plot(np.rad2deg(self.icang), self.RE, color='red', label="Reflect_Energy_Ratio")
+        plt.plot(np.rad2deg(self.icang), self.TE, color='blue', label="Trans_Energy_Ratio")
+        plt.legend()
+        plt.xlabel("Incident Angle")
+        plt.ylabel("R/T Energy Ratio")
         plt.show()
+
+def f(x):
+    return np.sqrt(x.real ** 2 + x.imag ** 2)
         
 
